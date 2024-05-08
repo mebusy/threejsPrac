f1238afb3e3ff401ada1e17887fe7244b0f40aee
Qi Binyi <qibinyi@Qis-Mac-mini.local>
2024-05-08 22:35:57 +0800

    init

--- init3jsProj.sh
+++ init3jsProj.sh
@@ -59,7 +59,7 @@ import * as THREE from 'three';
 
 const renderer = new THREE.WebGLRenderer();
 
-render.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
