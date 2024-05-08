a3b2b49d1fd01fc718a4257aa899882a98a91f6f
Qi Binyi <qibinyi@Qis-Mac-mini.local>
2024-05-08 23:15:11 +0800

    orbit

--- tutor/src/js/scripts.js
+++ tutor/src/js/scripts.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+// OrbitControls
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // renderer
 const renderer = new THREE.WebGLRenderer()
@@ -9,6 +11,10 @@ document.body.appendChild(renderer.domElement)
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 2, 5)
+
+// orbit controls
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.update()
 
 // show axis
 const axesHelper = new THREE.AxesHelper(5)
